9bc46ee4cafa1a094a93226250f02d020f10773a
Ving <vt3839796@gmail.com>
2023-10-11 14:21:05 +0700

    Fix dang nhap

--- TrangChu/js/dangky.js
+++ TrangChu/js/dangky.js
@@ -1,12 +1,17 @@
+var openSignUp = document.querySelector('.modal-dangky')
+var modal = document.querySelector('.dangky')
+var iconClose = document.querySelector('.modal-header i')
 
-var btnOpen = document.querySelector('.dangky')
-var btnClose = document.querySelector('.modal-header i')
-
-function toggleModal (e) {
+function toggleModal(e) {
     console.log(e.target)
     modal.classList.toggle('hide')
 }
 
-btnOpen.addEventListener('click', toggleModal)
-btnClose.addEventListener('click', toggleModal)
+openSignUp.addEventListener('click', toggleModal)
+iconClose.addEventListener('click', toggleModal)
+modal.addEventListener('click', function (e) {
+    if (e.target == e.currentTarget) {
+        toggleModal()
+    }
+})
 
